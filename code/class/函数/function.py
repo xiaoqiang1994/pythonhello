b521695a2da2c92ff21a32c5182d02f48cbca7a7
xiaoqiang1994 <1114623681@qq.com>
2019-05-28 19:39:17 +0800

    c=30

---
+++ code/class/函数/function.py
@@ -0,0 +1,16 @@
+print(abs(100))
+print(abs(-20))
+
+#abs(1,20) 传入参数不对会包TypeEerror
+print(max(1,2))
+int ('123')
+float(12.34)
+#str  bool
+
+a = abs
+print(a(-1))
+
+n1 = 255
+n2 = 1000
+print(hex(n1))
+print(hex(n2))
